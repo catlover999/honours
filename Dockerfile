18d55a7088825fc0f825ff769c3d66da62ebee6c
Julien Acker <work@julienacker.dev>
2024-03-17 21:07:58 +0000

    Implimented Toml-based configuration. Used rv's Laplace implimention

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,14 @@
 # 1. Build stages for filter_dp. Using cargo-chef for optimal layering
-FROM docker.io/rust:latest AS chef 
+FROM docker.io/rust:latest AS builder 
 RUN cargo install cargo-chef 
 RUN rustup target add wasm32-wasi
 WORKDIR /filter_dp
 
-FROM chef AS planner
 COPY filter_dp/Cargo.toml filter_dp/Cargo.lock ./
 COPY filter_dp/src src
 RUN cargo chef prepare --recipe-path recipe.json
 
-FROM chef AS builder
-COPY --from=planner /filter_dp/recipe.json recipe.json
 RUN cargo chef cook --release --target wasm32-wasi --recipe-path recipe.json
-COPY filter_dp/Cargo.toml filter_dp/Cargo.lock ./
-COPY filter_dp/src src
 RUN cargo build --release --target wasm32-wasi
 
 # 2. Fluent Bit Stage
--- filter_dp/src/lib.rs
+++ filter_dp/src/lib.rs
@@ -1,9 +1,11 @@
+use serde::Deserialize;
 use serde_json::{json, Value};
-use chrono::{Utc, TimeZone, LocalResult, prelude::DateTime};
-use std::slice;
-use std::os::raw::c_char;
-use std::fs;
-use std::io::{Read, Write};
+use chrono::{Utc, TimeZone};
+use std::{collections::HashMap, slice, os::raw::c_char, str};
+use rv::{dist::Laplace, traits::Rv};
+use std::{fs::File, io::Read, path::Path};
+use rand::thread_rng;
+use toml::from_str;
 
 #[no_mangle]
 pub extern "C" fn filter_dp(
@@ -16,23 +18,65 @@ pub extern "C" fn filter_dp(
 ) -> *const u8 {
     
     // Process tag and record
-    let tag_str: &str = std::str::from_utf8( unsafe { std::slice::from_raw_parts(tag as *const u8, tag_len as usize) } ).expect("Invalid UTF-8 in tag");
-    let message: Value = serde_json::from_slice( unsafe { slice::from_raw_parts(record as *const u8, record_len as usize) } ).expect("Invalid JSON in record");
+    let tag_str: String = str::from_utf8( unsafe { slice::from_raw_parts(tag as *const u8, tag_len as usize) } ).expect("Invalid UTF-8 in tag").to_string();
+    let records: Value = serde_json::from_slice( unsafe { slice::from_raw_parts(record as *const u8, record_len as usize) } ).expect("Invalid JSON in record");
     
+    // Apply noise to the records
+    let mut noisy_records: Value = apply_noise(&tag_str, records);
+
     // Process time value. Not used by filter but is required to be passed back
-    let dt_result: LocalResult<DateTime<Utc>> = Utc.timestamp_opt(time_sec as i64, time_nsec);
-    let dt: DateTime<Utc> = match dt_result {
-        chrono::LocalResult::Single(dt) => dt,
-        _ => panic!("Invalid timestamp"), // Handle the None case or ambiguity
-    };
-    let time: String = dt.format("%Y-%m-%dT%H:%M:%S.%9f %z").to_string();
+    let time: String = Utc.timestamp_opt(time_sec as i64, time_nsec)
+    .single()
+    .expect("Invalid timestamp")
+    .format("%Y-%m-%dT%H:%M:%S.%9f %z")
+    .to_string();
 
-    let message: Value = json!({
-        "time": time,
-        "tag": tag_str,
-    });
+    if let Value::Object(ref mut map) = noisy_records {
+        map.insert("time".to_string(), Value::String(time));
+        map.insert("tag".to_string(), Value::String(tag_str));
+    }
 
+  noisy_records.to_string()
+  .as_ptr()
+}
 
-  let buf: String = message.to_string();
-  buf.as_ptr()
+#[derive(Deserialize)]
+struct NoiseSetting {
+    b: f64,
+    mu: f64,
+}
+
+fn apply_noise(filename: &String, mut message: Value) -> Value {
+    let filepath: String = format!("{}.toml", filename);
+
+    // Check if there is a configuration file for the given tag
+    if Path::new(&filepath).exists() {
+        let mut contents = String::new();
+        // Attempt to open and read the file into a string
+        match File::open(&filepath).and_then(|mut file| file.read_to_string(&mut contents)) {
+            Ok(_) => {
+                // Attempt to parse the TOML contents into a TomlValue
+                if let Ok(decoded) = from_str::<HashMap<String, NoiseSetting>>(&contents) {
+                    // Iterate through the message object if it's a JSON object
+                    if let Value::Object(obj) = &mut message {
+                        for (record_key, record_value) in obj.iter_mut() {
+                            // Check if the record_key's value is numeric
+                            if let Some(x) = record_value.as_f64() {
+                                // Check if the record_key has a valid entry in the TOML data
+                                if let Some(setting) = decoded.get(record_key) {
+                                    // If the setting's b and mu are valid, calculate noise
+                                    let laplace: Laplace = Laplace::new(setting.mu, setting.b).expect("Invalid Laplace parameters");
+                                    let mut rng = thread_rng();
+                                    let noise: f64 = laplace.draw(&mut rng);
+                                    *record_value = json!(x + noise); // Update the value with noise applied
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            Err(_) => {} // If there's an error opening or reading the file, do nothing
+        }
+    }
+    message
 }
